aa5815b96d61df4759aac2e8ba7fe4894e13a284
mailtogauravseth <gaurav.seth@digital.hmrc.gov.uk>
2019-06-25 17:32:14 +0100

    Removed reference to Non CSP

--- src/test/scala/model/models.scala
+++ src/test/scala/model/models.scala
@@ -1,9 +1,8 @@
 package model
 
 sealed abstract class User(val token: String, val version: String){
-  require(Seq("CSP", "Non CSP").contains(token))
-  require(Seq("1.0", "2.0", "3.0").contains(version))
+  require(Seq("CSP").contains(token))
+  require(Seq("1.0").contains(version))
 }
 
 sealed case class Csp(override val version: String) extends User("CSP", version)
-sealed case class NonCsp(override val version: String) extends User("Non CSP", version)
