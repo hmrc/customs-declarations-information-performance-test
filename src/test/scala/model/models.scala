349b7efeb15e65298aa8aaf5136aac8d4cc6be6a
mailtogauravseth <gaurav.seth@digital.hmrc.gov.uk>
2019-06-25 22:40:00 +0100

    Setting load at 50 rps

--- src/test/scala/model/models.scala
+++ src/test/scala/model/models.scala
@@ -1,8 +1,6 @@
 package model
 
 sealed abstract class User(val token: String, val version: String){
-  require(Seq("CSP").contains(token))
-  require(Seq("1.0").contains(version))
 }
 
 sealed case class Csp(override val version: String) extends User("CSP", version)
